fa458017d52b49edf7de501ef059942a66254269
Berny Retana <berretan@crsjopgp0223.global.publicisgroupe.net>
2016-04-29 16:21:43 -0600

    about to add server side auth

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
+// var angularFilesort = require('gulp-angular-filesort');
+// var naturalSort = require('gulp-natural-sort');
+// var inject = require('gulp-inject');
 
 gulp.task('sass', function() {
   gulp.src('public/stylesheets/style.scss')
@@ -12,5 +15,19 @@ gulp.task('sass', function() {
 gulp.task('watch', function() {
   gulp.watch('public/stylesheets/*.scss', ['sass']);
 });
+
+// gulp.task('index', function () {
+//   var target = gulp.src('./public/index.html');
+//   // It's not necessary to read the files (will speed up things), we're only after their paths:
+//   var sources = gulp.src(
+//       ['./**/*.js', './**/*.css'],
+//       {
+//         cwd: __dirname + "/public/",
+//       }
+//     );
+
+//   return target.pipe(inject(sources, { addRootSlash: false }))
+//     .pipe(gulp.dest('./public'));
+// });
 
 gulp.task('default', ['sass', 'watch']);
